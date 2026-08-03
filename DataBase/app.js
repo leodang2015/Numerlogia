6d76b660efb1af5615722961696cc07c2c1f77f1
leodang2015 <leodang2015@gmail.com>
2026-08-03 11:14:01 -0500

    Update app.js

--- DataBase/app.js
+++ DataBase/app.js
@@ -1,13 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
-import { x } from "./cnxmongo.js";
+import { x } from "../cnxmongo.js";
 
-// Cargar variables de entorno primero
 dotenv.config();
 
 const app = express();
 
-// Ejecutar la conexión a MongoDB
 x();
 
 const PORT = process.env.PORT 
