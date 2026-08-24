5ddaa70392b4d9324824aab0c141f9deedf5742d
leodang2015 <leodang2015@gmail.com>
2026-08-24 11:21:52 -0500

    Delete routers directory

--- routers/NumerologyProfiles.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { actualizarNumerologyProfile, crearNumerologyProfile, eliminarNumerologyProfile, listarNumerologyProfiles, obtenerNumerologyProfile } from "../controllers/NumerologyProfiles.js";
-const router = Router();
-router.post("/", crearNumerologyProfile);
-router.get("/:id", obtenerNumerologyProfile);
-router.get("/", listarNumerologyProfiles);
-router.put("/:id", actualizarNumerologyProfile);
-router.delete("/:id", eliminarNumerologyProfile);
-export default router;
