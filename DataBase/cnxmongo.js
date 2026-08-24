700ece6a0c99d5d6aa3d0ddd1942f5c6affa2cf0
leodang2015 <leodang2015@gmail.com>
2026-08-24 11:21:28 -0500

    Delete DataBase directory

--- DataBase/cnxmongo.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-export const x = async () => {
-        await mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/trabajodenumologia");
-        console.log("Conexión exitosa a MongoDB");
-
-};
