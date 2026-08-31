1e8e11eb48eca1e9cc0a5125423192220d68c884
leodang2015 <leodang2015@gmail.com>
2026-08-31 11:09:59 -0500

    Add JWT validation to Readings routes

--- src/routers/Readings.js
+++ src/routers/Readings.js
@@ -12,13 +12,14 @@ import {
   idValidator 
 } from "../validators/Readings.js";
 import { validarCampos } from "../middlewares/validarCampos.js";
+import { validarJWT } from "../middlewares/Webtoken.js";
 
 const router = Router();
 
-router.get("/", listarReadings);
-router.post("/", crearReadingValidator, validarCampos, crearReading);
-router.get("/:id", idValidator, validarCampos, obtenerReading);
-router.put("/:id", idValidator, actualizarReadingValidator, validarCampos, actualizarReading);
-router.delete("/:id", idValidator, validarCampos, eliminarReading);
+router.get("/", validarJWT, listarReadings);
+router.post("/", validarJWT, crearReadingValidator, validarCampos, crearReading);
+router.get("/:id", validarJWT, idValidator, validarCampos, obtenerReading);
+router.put("/:id", validarJWT, idValidator, actualizarReadingValidator, validarCampos, actualizarReading);
+router.delete("/:id", validarJWT, idValidator, validarCampos, eliminarReading);
 
 export default router;
